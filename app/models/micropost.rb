5f4cde0d241250eb70548fd43c97ea69de49fc94
Yiming Ma <ian.fresn.ma@gmail.com>
2018-03-06 18:19:34 +0000

    ready to deploy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
-  validates :content,length:{ maximum:140}
+  belong_to :user
+  validates :content,length:{ maximum:140},
+                     presence: true
 end
